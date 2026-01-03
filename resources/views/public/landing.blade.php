7ae30bdcbebc916716e4e44a4454aee6fa85b93d
ahmed-mostafa1 <ahmed.code.dev@gmail.com>
2026-01-03 23:06:29 +0200

    started adding icons from pdf

--- resources/views/public/landing.blade.php
+++ resources/views/public/landing.blade.php
@@ -3010,9 +3010,7 @@
               @endphp
               <div class="goal-card" data-animate>
                 <div class="goal-icon">
-                  <svg class="icon" viewBox="0 0 24 24">
-                    <path d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 1 1 7.072 0l-.548.547A3.374 3.374 0 0 0 14 18.469V19a2 2 0 1 1-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547Z" />
-                  </svg>
+                  <img src="{{ asset('img/icons/star.png') }}" alt="Goal Icon" style="width: 40px; height: 40px;">
                 </div>
                 <div>
                   <h3 class="goal-title" data-en="{{ e($goalTitle['en']) }}" data-ar="{{ e($goalTitle['ar']) }}">{{ $goalTitle['text'] }}</h3>
