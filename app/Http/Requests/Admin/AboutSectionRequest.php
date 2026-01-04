3f15ce40ef4344b0ed907ac6213467fe51afe6f0
ahmed-mostafa1 <ahmed.code.dev@gmail.com>
2026-01-04 20:29:46 +0200

    made admin dashboard fields nullable

--- app/Http/Requests/Admin/AboutSectionRequest.php
+++ app/Http/Requests/Admin/AboutSectionRequest.php
@@ -18,19 +18,20 @@ class AboutSectionRequest extends FormRequest
         }
 
         return [
-            'title.en' => ['required', 'string'],
+            'title.en' => ['nullable', 'string'],
             'title.ar' => ['nullable', 'string'],
-            'mission.title.en' => ['required', 'string'],
+            'mission.title.en' => ['nullable', 'string'],
             'mission.title.ar' => ['nullable', 'string'],
-            'mission.paragraphs' => ['required', 'array', 'min:1'],
-            'mission.paragraphs.*.en' => ['required', 'string'],
+            'mission.paragraphs' => ['nullable', 'array', 'min:1'],
+            'mission.paragraphs.*.en' => ['nullable', 'string'],
             'mission.paragraphs.*.ar' => ['nullable', 'string'],
-            'goals' => ['required', 'array', 'min:1'],
-            'goals.*.title.en' => ['required', 'string'],
+            'goals' => ['nullable', 'array', 'min:1'],
+            'goals.*.title.en' => ['nullable', 'string'],
             'goals.*.title.ar' => ['nullable', 'string'],
-            'goals.*.description.en' => ['required', 'string'],
+            'goals.*.description.en' => ['nullable', 'string'],
             'goals.*.description.ar' => ['nullable', 'string'],
             'background_video_file' => ['nullable', 'file', 'mimetypes:video/mp4,video/webm', 'max:51200'],
         ];
     }
 }
+
