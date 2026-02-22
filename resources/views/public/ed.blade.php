29c0b8e00633285caec309d026a39d964726a5a5
Ahmed Mostafa <ahmed.code.dev@gmail.com>
2026-02-22 12:11:08 +0200

    upgrades

--- resources/views/public/ed.blade.php
+++ resources/views/public/ed.blade.php
@@ -836,9 +836,8 @@
                                 <img src="{{ asset('img/exsponsor/13.jpg') }}" style="background-color: #fff;" alt="Authority">
                             </div>
                             <div class="sponsor-item">
-                                <img src="{{ asset('img/exsponsor/14.jpg') }}" style="background-color: #fff;" alt="Authority">
+                                <img src="{{ asset('img/exsponsor/14-v2.jpg') }}" style="background-color: #fff;" alt="Authority">
                             </div>
-                            
                             <!-- Duplicate Slider Items for Infinite Loop -->
                            <div class="sponsor-item">
                                 <img src="{{ asset('img/exsponsor/1.jpg') }}" alt="IEC Logo">
@@ -880,7 +879,7 @@
                                 <img src="{{ asset('img/exsponsor/13.jpg') }}" style="background-color: #fff;" alt="Authority">
                             </div>
                             <div class="sponsor-item">
-                                <img src="{{ asset('img/exsponsor/14.jpg') }}" style="background-color: #fff;" alt="Authority">
+                                <img src="{{ asset('img/exsponsor/14-v2.jpg') }}" style="background-color: #fff;" alt="Authority">
                             </div>
                             <!-- end of duplicate -->
                         </div>
